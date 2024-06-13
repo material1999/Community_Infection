1d7553d672b8c2d9db26d3a2819428af245d09bb
Máté Vass <vass.mate99@gmail.com>
2024-06-13 16:10:49 +0200

    older and new version of communities gridsearch

--- general_communities_gridsearch_timeout.py
+++ general_communities_gridsearch_timeout.py
@@ -2,47 +2,67 @@ from pytictoc import TicToc
 import pandas as pd
 import networkx as nx
 import statistics
+from threading import Timer
+
+
+def continue_loop():
+    global flag_continue
+    flag_continue = True
+
 
 graphs = 1
 infection_model = "cascade"
+timeout = 5
 
 max_community_size = 20
-connected_percent = [0.4, 0.45, 0.5, 0.55]
-times_average = [3.5, 4, 4.5, 5]
+connected_percent = [0.4, 0.45, 0.5, 0.55, 0.6]
+times_average = [4, 4.5, 5, 5.5, 6]
 
 t = TicToc()
 runtimes_path = "runtimes/communities_gridsearch/" + infection_model + ".txt"
 with open(runtimes_path, "w") as runtimes_output:
-    runtimes_output.write("Runtimes (s) - max_community_size: " + str(max_community_size) + "\n")
+    runtimes_output.write("Runtimes (s) - max_community_size: " + str(max_community_size) +
+                          " - timeout (s): " + str(timeout) + "\n")
 
 for graph_num in range(1, graphs + 1, 10):
+
+    # Read infection graph file
+    print("##################################################")
+    print("Reading infection graph #" + str(graph_num))
+    graph_path = "results/infection_graphs/" + infection_model + "/" + str(graph_num) + ".csv"
+    graph_df = pd.read_csv(graph_path, sep=";")
+
+    # df = pd.read_csv(graph_path, sep=";")
+    # graph_df = df[df['edgeweight'] >= 0.05]
+
+    # print(graph_df)
+    # print(graph_df._get_value(0, "edgeweight"))
+
+    G = nx.DiGraph()
+    for row in graph_df.itertuples():
+        G.add_edge(int(row[1]), int(row[2]), weight=float(row[3]))
+    # print(G)
+
+    mean_edge_weight = statistics.mean(weight for _, _, weight in G.edges(data='weight'))
+    # print(mean_edge_weight)
+
     for c_p in connected_percent:
         for t_a in times_average:
 
+            flag_continue = False
+
+            timer = Timer(timeout, continue_loop)
+            timer.start()
+
+            ##if c_p == 0.4 and t_a == 4:
+                ##continue
+
             t.tic()
 
-            # Read infection graph file
-            print("##################################################")
-            print("Reading infection graph #" + str(graph_num))
-            graph_path = "results/infection_graphs/" + infection_model + "/" + str(graph_num) + ".csv"
-            graph_df = pd.read_csv(graph_path, sep=";")
-
-            # df = pd.read_csv(graph_path, sep=";")
-            # graph_df = df[df['edgeweight'] >= 0.05]
-
-            # print(graph_df)
-            # print(graph_df._get_value(0, "edgeweight"))
-
-            G = nx.DiGraph()
-            for row in graph_df.itertuples():
-                G.add_edge(int(row[1]), int(row[2]), weight=float(row[3]))
-            # print(G)
-
-            mean_edge_weight = statistics.mean(weight for _, _, weight in G.edges(data='weight'))
-            # print(mean_edge_weight)
             communities = list({i} for i in range(1, 1000 + 1))
             # print(communities)
 
+            print("##################################################")
             print("Parameters - connected_percent: " + str(c_p) + " - times_average: " + str(t_a))
             print("Searching for communities in graph #" + str(graph_num))
             for community_size in range(2, max_community_size + 1):
@@ -51,6 +71,8 @@ for graph_num in range(1, graphs + 1, 10):
                 to_add = list()
                 to_delete = list()
                 for community in communities:
+                    if flag_continue:
+                        break
                     if len(community) != community_size - 1:
                         continue
                     neighbors = set()
@@ -78,6 +100,8 @@ for graph_num in range(1, graphs + 1, 10):
                                 to_delete.append(community)
                             if set(community_temp) not in to_add and set(community_temp) not in communities:
                                 to_add.append(set(community_temp))
+                if flag_continue:
+                    break
                 if last_iteration == 0:
                     print("Break at community size: " + str(community_size))
                     break
@@ -86,6 +110,13 @@ for graph_num in range(1, graphs + 1, 10):
                 communities = [item for item in communities if item not in to_delete]
                 communities.extend(to_add)
                 # print(communities)
+
+            if flag_continue:
+                print("xxxxxxxxxx")
+                with open(runtimes_path, "a") as runtimes_output:
+                    runtimes_output.write(str(graph_num) + "_" + str(int(c_p * 100)) + "%_" + str(t_a) + "x: "
+                                          + "xxxxxxxxxx" + "\n")
+                continue
 
             communities = [item for item in communities if len(item) != 1]
             # print(communities)
@@ -105,5 +136,7 @@ for graph_num in range(1, graphs + 1, 10):
             with open(runtimes_path, "a") as runtimes_output:
                 runtimes_output.write(str(graph_num) + "_" + str(int(c_p*100)) + "%_" + str(t_a) + "x: "
                                       + str(runtime) + "\n")
+
+            timer.cancel()
 
             # print("##################################################")
