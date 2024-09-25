825d40059d397cd05f7931e8c21c22dc57733d5a
Máté Vass <vass.mate99@gmail.com>
2024-09-25 14:29:07 +0200

    community finder runtime summary

---
+++ summary_community_runtime.py
@@ -0,0 +1,15 @@
+lines = 2700
+infection_model = "onlylistenonce"
+
+input_path = ("_office/runtimes/communities_gridsearch/" + infection_model + ".txt")
+output_path = ("_office/runtimes/communities_gridsearch/summary/" + infection_model + ".csv")
+
+with open(input_path, 'r') as input, open(output_path, 'w') as output:
+
+    next(input)
+
+    for graph_num in range(1, lines + 1):
+        runtime = str(input.readline().strip().split(': ')[1])
+        output.write(runtime + ";")
+        if graph_num % 25 == 0:
+            output.write("\n")
