74bb153521ef0511ca6c52dddf9243fe6a93e829
Máté Vass <vass.mate99@gmail.com>
2024-09-29 19:26:59 +0200

    updated visualizations

--- visualizations/visualizations.R
+++ visualizations/visualizations.R
@@ -1,19 +1,29 @@
-setwd("~/Documents/Projects/CommunityInfectionRework/visualizations/")
+library(dplyr)
 
-greedy_full <- data.frame(matrix(ncol = 3, nrow = 0))
+setwd("~/Documents/Projects/CommunityInfectionRework/")
+
+max_in_row <- function(row) {
+  numeric_row <- as.numeric(suppressWarnings(as.numeric(row)))
+  if (all(is.na(numeric_row))) {
+    return(c(NA, NA))
+  }
+  max_value <- max(numeric_row, na.rm = TRUE)
+  max_col <- names(row)[which.max(numeric_row)]
+  return(c(max_value, max_col))
+}
 
 greedy_full <- data.frame(
-  cascade = read.csv("results/greedy_full/cascade.csv", header = FALSE)[, 1],
-  threshold = read.csv("results/greedy_full/threshold.csv", header = FALSE)[, 1],
-  onlylistenonce = read.csv("results/greedy_full/onlylistenonce.csv", header = FALSE)[, 1]
+  cascade = read.csv("_office/results/greedy_full/summary/cascade.csv", header = FALSE)[, 1],
+  threshold = read.csv("_office/results/greedy_full/summary/threshold.csv", header = FALSE)[, 1],
+  onlylistenonce = read.csv("_office/results/greedy_full/summary/onlylistenonce.csv", header = FALSE)[, 1]
   )
 
 greedy_narrow_20_cascade <- read.csv(
-  "results/greedy_narrow_20/cascade.csv", sep = ";", header = TRUE
-  )
-greedy_narrow_20_threshold <- read.csv(
-  "results/greedy_narrow_20/threshold.csv", sep = ";", header = TRUE
-  )
-greedy_narrow_20_onlylistenonce <- read.csv(
-  "results/greedy_narrow_20/onlylistenonce.csv", sep = ";", header = TRUE
-  )
+  "_office/results/greedy_narrow_20/summary/cascade.csv", sep = ";", header = TRUE, check.names = FALSE
+) %>% select(-ncol(.))
+
+max_greedy_narrow_20_cascade <- as.data.frame(t(apply(greedy_narrow_20_cascade, 1, max_in_row)))
+colnames(max_greedy_narrow_20_cascade) <- c("Max_Value", "Max_Column")
+
+labels_max_greedy_narrow_20_cascade <- as.data.frame(table(max_greedy_narrow_20_cascade$Max_Column))
+colnames(labels_max_greedy_narrow_20_cascade) <- c("Column", "Count")
