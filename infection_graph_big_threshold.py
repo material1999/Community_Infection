27a4fd24c4cd404f44caa6c907eda1ecfb74dc0c
Máté Vass <vass.mate99@gmail.com>
2024-11-14 14:43:23 +0100

    big infection graphs

--- infection_graph_big_threshold.py
+++ infection_graph_big_threshold.py
@@ -1,14 +1,14 @@
 from pytictoc import TicToc
 import pandas as pd
 import networkx as nx
-import numpy as np
+import random
 
 graphs = ["Cit-HepPh", "Email-EuAll", "soc-Epinions1"]
-instances_infection_graph = 1000
+instances_infection_graph = 10000
 cutoff = 2
 
 t = TicToc()
-runtimes_path = "runtimes/infection_graphs_big/cascade.txt"
+runtimes_path = "runtimes/infection_graphs_big/threshold.txt"
 with open(runtimes_path, "w") as runtimes_output:
     runtimes_output.write("Runtimes (s) - instances: " + str(instances_infection_graph)
                           + " - cutoff: " + str(cutoff) + "\n")
@@ -21,31 +21,61 @@ for graph in graphs:
     print("##################################################")
     print("Reading graph " + graph)
     graph_path = "data/big_graphs_weighted/" + graph + ".csv"
-    graph_df = pd.read_csv(graph_path, sep=";", header=None)
+    graph_df = pd.read_csv(graph_path, sep=";")
+    graph_df.columns = ["V1", "V2", "edgeweight"]
+    vertex_path = "data/big_graphs_vertices/" + graph + ".csv"
+    vertex_df = pd.read_csv(vertex_path, sep=";", names=["vertex", "weight"])
 
     all_nodes = pd.concat([graph_df['V1'], graph_df['V2']]).unique().tolist()
 
     # print(graph_df)
     # print(graph_df._get_value(0, "edgeweight"))
+    # print(vertex_df)
+    # print(vertex_df._get_value(0, "weight"))
+
+    incoming_edges_dict = dict()
+
+    for row in vertex_df.itertuples():
+
+        weights = graph_df.loc[graph_df['V2'] == row[1]]["edgeweight"].tolist()
+        from_vertex = graph_df.loc[graph_df['V2'] == row[1]]["V1"].tolist()
+        if sum(weights) < 1:
+            weights.append(1 - sum(weights))
+            from_vertex.append(None)
+        else:
+            weights = [float(i) / sum(weights) for i in weights]
+
+        # print(row)
+        # print(graph_df.loc[graph_df['V2'] == row[1]])
+        # print(weights)
+        # print(from_vertex)
+
+        incoming_edges_dict[row[1]] = [from_vertex, weights]
+
+    # print(incoming_edges_dict)
 
     reached_nodes = dict()
 
-    print("Creating instances for graph " + graph)
+    print("Creating instances for graph # " + graph)
 
     for instance_num in range(1, instances_infection_graph + 1):
 
         # Create instances
-        # print("graph: " + str(graph_num) + " --- instance: " + str(instance_num)
-        #       + "/" + str(instances_infection_graph))
+        print("graph: " + graph + " --- instance: " + str(instance_num)
+              + "/" + str(instances_infection_graph))
 
         G = nx.DiGraph()
         G.add_nodes_from(all_nodes)
 
-        for row in graph_df.itertuples():
-            r = np.random.rand()
-            if r < row[3]:
-                G.add_edge(int(row[1]), int(row[2]))
-        # print(G)
+        for vertex in all_nodes:
+
+            choice = random.choices(incoming_edges_dict[vertex][0],
+                                    weights=incoming_edges_dict[vertex][1])[0]
+            # choice = np.random.choice(incoming_edges_dict[vertex][0],
+            #                           p=incoming_edges_dict[vertex][1])
+            # print(choice)
+            if choice is not None:
+                G.add_edge(choice, vertex)
 
         # General solution for any cutoff value
 
@@ -84,16 +114,14 @@ for graph in graphs:
 
     # print(reached_nodes)
 
-    infection_graph_path = "results/infection_graphs_big/cascade/" + graph + ".csv"
+    infection_graph_path = "results/infection_graphs_big/threshold/" + graph + ".csv"
     with open(infection_graph_path, 'w') as infection_graph_output:
         infection_graph_output.write("\"V1\";\"V2\";\"edgeweight\"\n")
         for key, value in reached_nodes.items():
             infection_graph_output.write(str(key) + ";" + str(value / instances_infection_graph) + "\n")
 
-    print("Infection graph " + graph + " created")
+    print("Infection graph # " + graph + " created")
     runtime = t.tocvalue()
     print("Runtime:", runtime, "s")
     with open(runtimes_path, "a") as runtimes_output:
         runtimes_output.write(graph + ": " + str(runtime) + "\n")
-
-    print("##################################################")
